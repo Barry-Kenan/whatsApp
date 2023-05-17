6c2979403531c2e71c4e8f656da620ce72bb2348
Kenan <bary.berdiyev.97@mail.ru>
2023-05-17 20:00:50 +0700

    получение инишл пропсов

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,38 @@
-import { Button } from "@mui/material";
+import { Ijwt } from "@/interfaces/jwt.interface";
+import { ILogin } from "@/interfaces/login.interface";
+import { withLayout } from "@/layout/Layout";
+import { TopPage } from "@/pages-component";
+import axios from "axios";
+import { verify } from "jsonwebtoken";
+import { GetStaticProps } from "next";
 
-export default function Home() {
-    return (
-        <>
-            <h1>Hello Next</h1>
-            <Button variant="text">Text</Button>
-            <Button variant="contained">Contained</Button>
-            <Button variant="outlined">Outlined</Button>
-        </>
-    );
+function Home({ loginData }: HomeProps): JSX.Element {
+    return <TopPage data={loginData} />;
+}
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const { data } = await axios.get("http://localhost:3001/login");
+    let loginData = data;
+    if (data.id && data.token) {
+        const secret = process.env.NEXT_SECRET as string;
+        const id = verify(data.id, secret) as Ijwt;
+        const token = verify(data.token, secret) as Ijwt;
+        loginData = { id: id.name, token: token.name };
+        return {
+            props: {
+                loginData,
+            },
+        };
+    }
+    return {
+        props: {
+            loginData,
+        },
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    loginData: ILogin;
 }
