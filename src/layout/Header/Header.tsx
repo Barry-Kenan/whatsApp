e535a3df8d2043fcc4a47070b1e8a36f57cc892e
Kenan <bary.berdiyev.97@mail.ru>
2023-05-17 19:59:59 +0700

    кнопка logout

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -4,11 +4,32 @@ import styles from "./Header.module.scss";
 import cn from "classnames";
 import { Button } from "@mui/material";
 import Link from "next/link";
+import axios from "axios";
+import { useAppSelector } from "@/hooks/redux";
+import { useActions } from "@/hooks/actions";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
+    const { isAuth } = useAppSelector((state) => state.auth);
+    const { logout } = useActions();
+    const handleSubmit = () => {
+        axios.post(
+            "api/auth/logout",
+            {},
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        logout();
+    };
+
     return (
         <header className={cn(className, styles.header)} {...props}>
-            <nav>
+            <nav className={styles.nav}>
+                <WhatsAppIcon fontSize="large" sx={{ color: "white" }} />
                 <Link href="/">
                     <Button variant="text">Home</Button>
                 </Link>
@@ -16,6 +37,17 @@ const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
                     <Button variant="text">Chat</Button>
                 </Link>
             </nav>
+            {isAuth ? (
+                <div className={styles.logout}>
+                    <AccountCircleIcon
+                        fontSize="large"
+                        sx={{ color: "white" }}
+                    />
+                    <Button onClick={handleSubmit}>Logout</Button>
+                </div>
+            ) : (
+                <></>
+            )}
         </header>
     );
 };
