4a33f373b803b9ba4f8f481b2cd7f0cc430feb98
Kenan <bary.berdiyev.97@mail.ru>
2023-05-17 20:05:05 +0700

    запрос на api

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,14 +3,16 @@ import React, { useState } from "react";
 import styles from "./LoginForm.module.scss";
 import cn from "classnames";
 import { useForm } from "react-hook-form";
-import { ILoginForm, ILoginSentResponse } from "./ILoginForm.interface";
+import { ILoginForm } from "./ILoginForm.interface";
 import axios from "axios";
 import CloseIcon from "@mui/icons-material/Close";
 import { useActions } from "@/hooks/actions";
 import { LoginFormProps } from "./LoginForm.props";
+import { useRouter } from "next/router";
 
 const LoginForm = ({ className, ...props }: LoginFormProps) => {
-    const { auth } = useActions();
+    const { login: auth } = useActions();
+    const { push } = useRouter();
     const [error, setError] = useState<string>();
     const {
         register,
@@ -21,22 +23,20 @@ const LoginForm = ({ className, ...props }: LoginFormProps) => {
     } = useForm<ILoginForm>();
 
     const onSubmit = async (formData: ILoginForm) => {
-        try {
-            const { data } = await axios.get<ILoginSentResponse>(
-                `https://api.green-api.com/waInstance${formData.idInstance}/getStateInstance/${formData.apiTokenInstance}`
-            );
-            if (data.stateInstance == "authorized") {
-                auth(formData);
-                reset();
-            } else {
-                setError("Что-то пошло не так");
-            }
-        } catch (e) {
-            if (e instanceof Error) {
-                setError(e.message);
-            }
+        const login = await axios.post("api/auth/login", formData, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (login.data.message == "authorized") {
+            auth(formData);
+            reset();
+            push("/chat");
+        } else {
+            setError("Что-то пошло не так");
         }
     };
+
     return (
         <form
             onSubmit={handleSubmit(onSubmit)}
