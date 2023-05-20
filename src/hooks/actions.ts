8054c079b83034ca476fd34991e671f057e84ea3
Kenan <bary.berdiyev.97@mail.ru>
2023-05-20 19:42:18 +0700

    добавил экшины для работы с сообщениями

--- src/hooks/actions.ts
+++ src/hooks/actions.ts
@@ -1,8 +1,10 @@
 import { bindActionCreators } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { authActions } from "../store/auth/auth.slice";
+import { messagesActions } from "@/store/messages/messages.slice";
 const actions = {
     ...authActions,
+    ...messagesActions,
 };
 
 export const useActions = () => {
